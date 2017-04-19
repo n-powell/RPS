e0c11239fcbc549ac9a7f0168d9db660f9df251e
Nick Powell and Sowmya Dinavahi <nick@>
2017-04-19 15:01:14 -0700

    pass first spec

--- lib/rocksgame.rb
+++ lib/rocksgame.rb
@@ -1,1 +1,9 @@
-class rocksgame
+class String
+  define_method(:beats?) do |second|
+    if (self == "rock") && (second == "scissors")
+      true
+    elsif (self == "paper") && (second == "rock")
+      true
+    end
+  end
+end
--- spec/rocksgame_spec.rb
+++ spec/rocksgame_spec.rb
@@ -2,10 +2,18 @@ require ('rocksgame')
 require ('rspec')
 require ('./lib/rocksgame')
 
-  describe("#rocksgame") do
+  describe("String#beats") do
 
     it("returns true if rock beats scissors") do
       expect("rock".beats?("scissors")).to(eq(true))
+    end
+
+    it("returns true if paper beats rock") do
+      expect("paper".beats?("rock")).to(eq(true))
+    end
+
+    it("returns true if scissors beats paper") do
+      expect("scissors".beats?("paper")).to(eq(true))
     end
 
 end
